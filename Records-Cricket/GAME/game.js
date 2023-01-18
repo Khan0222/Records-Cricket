4ef1e03c67b1ca6e5c5f30482488cad398e5f3d8
Khan0222 <120516252+Khan0222@users.noreply.github.com>
2023-01-18 17:01:49 +0530

    Add files via upload

---
+++ Records-Cricket/GAME/game.js
@@ -0,0 +1,38 @@
+function ok() {
+  let Name = document.getElementById("input1").value;
+  let name = Name.toLowerCase();
+  // console.log(name);
+  let result = [];
+  if (name == "sheebu" || name == "shibu" || name == "tahir") {
+    result = ["Searching....................!!!..Bhkkk Sale PANAUTY"];
+  } else if (name == "faiz") {
+    result =
+      " Searching....................!!!..Sirf or Sirf Dawate Khane K Records hai...";
+  } else if (name == "minhaj") {
+    result =
+      "Searching....................!!!..Yhaa Chey Chkke Khane Walo k Records nhee likhe jate ";
+  } else if (name == "asad") {
+    result = ["Searching....................!!!..Tum Pehle J/S pr dhyaan do"];
+  } else if (name == "intezaar" || name == "intezar") {
+    result = ["Searching....................!!!..Right Arm full toss bowler"];
+  } else if (name == "zain") {
+    result = ["bumarah with speed 40km/hr"];
+  } else if (name == "rashid") {
+    result = ["abba nhee manege"];
+  } else if (name == "osama") {
+    result = ["6 ball pr 6 run to mar dete km se km"];
+  } else if (
+    name == "idrees" ||
+    name == "edris" ||
+    name == "edrees" ||
+    name == "idris"
+  ) {
+    result = ["buddhon k records nhee milege yhaa"];
+  } else {
+    result = ["abe jahil middle name likh bss"];
+  }
+
+  document.getElementById("answer").innerHTML = result;
+}
+
+
